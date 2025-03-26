445cf0ae7d0b644ac106298cde23a80ca1b5221f
RicJaquez <rjaquez@gmail.com>
2025-03-26 16:15:14 -0600

    removed commented lines and simpified general text methods

--- pages/shop_checkout_page.py
+++ pages/shop_checkout_page.py
@@ -8,12 +8,9 @@ class ShopCheckoutPage(CommonPage):
         self.driver = driver
     
     ##########################################
-    # LOCATORS from select product to checkout
+    # LOCATORS from product page to checkout
     ##########################################
-    # select_product = "add-to-cart-sauce-labs-fleece-jacket"
-    # select_product_2 = "add-to-cart-sauce-labs-onesie"
     shopping_cart = "//a[@class='shopping_cart_link']"
-    amount_locator = "//div[@class='inventory_item_price']"
     checkout_button = "//button[@id='checkout']"
 
     ############################################
@@ -28,173 +25,52 @@ class ShopCheckoutPage(CommonPage):
     thankyou_locator = "//h2[normalize-space()='Thank you for your order!']"
     back_locator = "back-to-products"
 
-    #####################################################
-    # Actions on Locators from select product to checkout
-    #####################################################
-    """
-    def clickSelectProduct(self):
-        self.elementClick(self.select_product_1)
-    
-    def clickShoppingCart(self):
-        self.elementClick(self.shopping_cart, locatorType="xpath")
-    
-    def clickCheckoutButton(self):
-        self.elementClick(self.checkout_button, locatorType="xpath")
-    
-    #######################################################
-    # Actions on Locators to introduce customer information
-    #######################################################
-    def sendFirstName(self, first_name):
-        self.elementSendKeys(first_name, self.first_name_locator)
-
-    def sendLastName(self, first_name):
-        self.elementSendKeys(first_name, self.last_name_locator)
-    
-    def sendPostalCode(self, first_name):
-        self.elementSendKeys(first_name, self.postal_code_locator)
-    
-    def clickContinueButton(self):
-        self.elementClick(self.continue_locator)
-    
-    def clickFinishButton(self):
-        self.elementClick(self.finish_locator, locatorType="xpath")
-    
-    def clickBackHomeButton(self):
-        self.elementClick(self.back_locator)
-    """
     ############################################################
     # Method that calls actions from select product to checkout
     ############################################################
     def selectProduct(self, clickLst):
-        # self.clickSelectProduct()
-        # self.elementClick(self.select_product)
         for element in clickLst:
             self.elementClick(element)
         time.sleep(1)
     
     def clickShoppingCartButton(self):
-        # self.clickShoppingCart()
         self.elementClick(self.shopping_cart, locatorType="xpath")
         time.sleep(1)
     
-    def getAmount(self):
-        amount = self.elementText(self.amount_locator, locatorType="xpath")
-        return amount
-    
     def selectCheckout(self):
-        # self.clickCheckoutButton()
         self.elementClick(self.checkout_button, locatorType="xpath")
     
     ############################################################
     # Method that calls actions to add customer information
     ############################################################
     def checkoutInformation(self, first_name, last_name, postal_code):
-        """
-        self.sendFirstName(first_name)
-        self.sendLastName(last_name)
-        self.sendPostalCode(postal_code)
-        """
         self.elementSendKeys(first_name, self.first_name_locator)
         self.elementSendKeys(last_name, self.last_name_locator)
         self.elementSendKeys(postal_code, self.postal_code_locator)
         time.sleep(1)
     
     def continueButton(self):
-        # self.clickContinueButton()
         self.elementClick(self.continue_locator)
         time.sleep(1)
     
     def checkoutFinish(self):
-        # self.clickFinishButton()
         self.elementClick(self.finish_locator, locatorType="xpath")
 
     def checkoutComplete(self):
-        # self.clickBackHomeButton()
         self.elementClick(self.back_locator)
     
     ######################################
-    # General Function to Verify Text
+    # General Text Methods
     ######################################
-    def verifyText(self, locator, locatorType="id"):
-        # return super().verifyText(locator, locatorType)
+    def getText(self, locator, locatorType):
+        txt = self.elementText(locator, locatorType)
+        return txt
+    
+    def verifyText(self, locator, locatorType):
         return self.verifyGeneralText(locator, locatorType)
     
-    ######################################
-    # Verify we landed at Products page after login
-    ######################################
-    """
-    def verifyProductsPage(self):
-        products = "//span[.='Products']"
-        result = self.isElementPresent(products, locatorType="xpath")
-        return result
-    """
-
-    ######################################
-    # Verify product added to the shopping cart
-    ######################################
-    """
-    def verifyShoppingCart(self):
-        shopping_cart_badge = "//span[@class='shopping_cart_badge']"
-        result = self.isElementPresent(shopping_cart_badge, locatorType="xpath")
-        return result
-    """
-
-    ######################################
-    # Verify the Your Cart text
-    ######################################    
-    """
-    def verifyAddToCart(self):
-        cart_locator = "//span[.='Your Cart']"
-        result = self.isElementPresent(cart_locator, locatorType="xpath")
-        return result
-    """
-
-    ######################################
-    # Verify the selected product text
-    ######################################
-    """
-    def verifyDescription(self):
-        description = "//div[.='Sauce Labs Fleece Jacket']"
-        result = self.isElementPresent(description, locatorType="xpath")
-        return result
-    """
-
-    ######################################
-    # Verify the Your Information text
-    ######################################
-    """
-    def confirmInformationPage(self):
-        confirm_page = "//span[.='Checkout: Your Information']"
-        result = self.isElementPresent(confirm_page, locatorType="xpath")
-        return result
-    """
-    
-    ######################################
-    # Verify the Checkout Overview text
-    ######################################    
-    """
-    def verifyCheckoutOverview(self):
-        overview_locator = "//span[.='Checkout: Overview']"
-        result = self.isElementPresent(overview_locator, locatorType="xpath")
-        return result
-    """
-
-    ######################################
-    # Verify the Checkout Complete text
-    ######################################   
-    """ 
-    def verifyCheckoutComplete(self):
-        complete_locator = "//span[.='Checkout: Complete!']"
-        result = self.isElementPresent(complete_locator, locatorType="xpath")
-        return result
-    """
-    
-    ######################################
-    # Verify the Thank You text
-    ######################################    
-    """
-    def verifyThankyou(self):
-        thankyou_locator = "//h2[normalize-space()='Thank you for your order!']"
-        result = self.isElementPresent(thankyou_locator, locatorType="xpath")
-        return result
-    """
+    def compareText(self, txt1, txt2):
+        if txt1 == txt2:
+            return True
+        else:
+            return False
